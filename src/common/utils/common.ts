a35c505569ac9ae7881a5054a058cb8c341c9192
e <k13772445714@163.com>
2022-04-17 18:31:28 +0800

    delete editor12

--- src/common/utils/common.ts
+++ src/common/utils/common.ts
@@ -47,3 +47,17 @@ export function getNextKey(map: Map<string, any>, key: string): string {
   }
   return ''
 }
+
+//字符串转HASH
+export function hashVal(string: string) {
+  var hash = 0,
+    i,
+    chr
+  if (string.length === 0) return hash
+  for (i = 0; i < string.length; i++) {
+    chr = string.charCodeAt(i)
+    hash = (hash << 5) - hash + chr
+    hash |= 0 // Convert to 32bit integer
+  }
+  return hash
+}
